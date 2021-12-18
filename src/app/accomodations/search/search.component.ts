5363556a8a6c635fbb2dcc6f00b26415c1d64e04
AstridPrlt <31550071+AstridPrlt@users.noreply.github.com>
2021-12-18 15:11:30 +0100

    work on switch small form / large form

--- src/app/accomodations/search/search.component.ts
+++ src/app/accomodations/search/search.component.ts
@@ -14,6 +14,11 @@ export class SearchComponent implements OnInit {
   listCities!: string[];
   citiesCount: number = 0;
 
+  location: HTMLElement | null | undefined;
+  guests: HTMLElement | null | undefined;
+  searchContainer: HTMLElement | null | undefined;
+  isSearchContainerSmall: boolean = true;
+
   searchForm = this.fb.group({
     location: ["All"],
     guestsAdults: ["1"],
@@ -29,9 +34,41 @@ export class SearchComponent implements OnInit {
     // this.searchForm;
 
     this.citiesCount = this.listCities.length;
+    this.location = document.getElementById("location");
+    this.guests = document.getElementById("guests");
+    this.searchContainer = document.getElementById("searchContainer");
+  }
 
-    const searchContainer = document.getElementById("searchContainer");
-    searchContainer != null ? searchContainer.style.height = `calc(175px + 37px * ${this.citiesCount})` : 'auto';
+  openSearchForm(): void {
+    if(this.isSearchContainerSmall) {
+      this.searchContainer?.classList.remove('small');
+      this.searchContainer?.classList.add('large');
+      this.searchContainer != null ? this.searchContainer.style.height = `calc(175px + 35px * ${this.citiesCount})` : 'auto';
+      this.isSearchContainerSmall = false;
+    }
+  }
+
+  closeSearchForm(): void {
+    // this.location?.children.item(1)?.classList.add('hide');
+    // this.guests?.children.item(1)?.classList.add('hide');
+    this.searchContainer?.classList.add('small');
+    this.searchContainer?.classList.remove('large');
+    this.searchContainer!.style.height = 'auto';
+    this.isSearchContainerSmall = true;
+  }
+
+  toggleActive(field: string): void {
+    if(field == "location") {
+      this.location?.classList.add('active');
+      this.location?.children.item(1)?.classList.remove('hide');
+      this.guests?.classList.remove('active');
+      this.guests?.children.item(1)?.classList.add('hide');
+    } else {
+      this.location?.classList.remove('active');
+      this.location?.children.item(1)?.classList.add('hide');
+      this.guests?.classList.add('active');
+      this.guests?.children.item(1)?.classList.remove('hide');
+    }
   }
 
   handleGuestsMinus() {
