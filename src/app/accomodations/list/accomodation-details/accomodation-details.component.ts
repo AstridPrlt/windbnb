db9d678a27eb5eb365438d5fc5a5c8015291b9fb
AstridPrlt <31550071+AstridPrlt@users.noreply.github.com>
2023-03-01 18:19:28 +0100

    accomodation details : add pictures

--- src/app/accomodations/list/accomodation-details/accomodation-details.component.ts
+++ src/app/accomodations/list/accomodation-details/accomodation-details.component.ts
@@ -13,6 +13,7 @@ import UnsplashImagesJson from '../../../../assets/unsplashImages.json';
 export class AccomodationDetailsComponent implements OnInit {
   details$!: Observable<Records>;
   unsplashImage!: string;
+  randomImages: string[] = [];
 
   showAllAmenities: boolean = false;
 
@@ -27,9 +28,31 @@ export class AccomodationDetailsComponent implements OnInit {
     this.details$ = this.service.getApiRecordDetails(id);
 
     console.log(this.details$.subscribe({next: res => console.log(res.record.fields)}));
+
+    this.getRandomImages();
   }
 
   toggleShowAllAmenities(): void {
     this.showAllAmenities = !this.showAllAmenities;
+  }
+
+  getRandomImages(): void {
+    let maxNumber = UnsplashImagesJson.length - 1;
+    let minNumber = 100;
+    let numberOfImages = 4;
+    let randomNumbers: number[] = [];
+    let randomAlreadyExists: boolean;
+
+    for (let index = 0; index < numberOfImages; index++) {
+      do {
+        let randomNb = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+        randomAlreadyExists = randomNumbers.includes(randomNb);
+        if(!randomAlreadyExists) {
+          randomNumbers.push(randomNb);
+          this.randomImages.push(UnsplashImagesJson[randomNb].urls.small);
+        }
+      }
+      while(randomAlreadyExists)
+    }
   }
 }
